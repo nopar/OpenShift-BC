13b2e2caf6702ae18d740a7ded7a8f11a092e2ba
Loucher <matus.svitek@gmail.com>
2014-05-03 15:23:50 +0200

    skuska REST service

---
+++ src/main/java/com/sprhib/restfull/service/ApplicationConfig.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sprhib.restfull.service;
+
+/**
+ *
+ * @author matus_000
+ */
+public class ApplicationConfig {
+    
+}
